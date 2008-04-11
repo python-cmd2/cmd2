dcfb091cc291ac8db9ea5d7ae3310c439a19e685
catherine <catherine@DellZilla.myhome.westell.com>
2008-04-11 08:56:54 -0400

    python 2.4 compatibility

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="cmd2",
-    version="0.2.1",
+    version="0.2.2",
     py_modules = ['cmd2','flagReader'],
     
     # metadata for upload to PyPI
