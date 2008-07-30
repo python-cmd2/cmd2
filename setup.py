fe21fc57b56606e4506803ece606af5b4394141e
catherine <catherine@localhost>
2008-07-30 12:41:14 -0400

    0.3.5

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="cmd2",
-    version="0.3.4.1",
+    version="0.3.5",
     py_modules = ['cmd2','flagReader','bootstrap'],
     
     # metadata for upload to PyPI
