af9677953228478d28b715a7f44283cb5fe64cd0
Catherine Devlin <catherine.devlin@gmail.com>
2008-11-20 16:35:35 -0500

    all but ignore seems to work

--- ignoreBug.py
+++ ignoreBug.py
@@ -1,5 +1,8 @@
 import pyparsing
 statementParser = pyparsing.Combine(pyparsing.Word(pyparsing.printables)('command') +
-                                            pyparsing.SkipTo('|' + pyparsing.stringEnd)('args') 
+                                            pyparsing.SkipTo('|' ^ pyparsing.stringEnd)('args') 
                                            )('statement')
 print statementParser.parseString('hello there /* you | fish */ box').dump()
+statementParser.ignore(pyparsing.cStyleComment)
+print statementParser.parseString('hello there /* you | fish */ box').dump()
+                       
