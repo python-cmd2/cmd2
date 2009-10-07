168376b8c5d244403de56f9b85b042a667550641
Catherine Devlin <catherine.devlin@gmail.com>
2009-10-07 13:33:58 -0400

    adding .get() to opts

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="cmd2",
-    version="0.5.5",
+    version="0.5.6",
     py_modules=["cmd2",],
     
     # metadata for upload to PyPI
