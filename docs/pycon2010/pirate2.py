a616b93ee3159cfddb561a03f515f329d00a1317
catherine <catherine@Drou>
2010-02-16 11:02:56 -0500

    begin Pycon talk

---
+++ docs/pycon2010/pirate2.py
@@ -0,0 +1,16 @@
+from cmd import Cmd
+# using ``do_`` methods
+
+class Pirate(Cmd):
+    gold = 10
+    def do_loot(self, arg):
+        'Seize booty frrrom a passing ship.'
+        self.gold += 1
+        print 'Now we gots {0} doubloons'.format(self.gold)
+    def do_drink(self, arg):
+        'Drown your sorrrows in rrrum.'
+        self.gold -= 1
+        print 'Now we gots {0} doubloons'.format(self.gold)
+
+pirate = Pirate()
+pirate.cmdloop()
---
+++ docs/pycon2010/pirate6.py
@@ -0,0 +1,33 @@
+from cmd2 import Cmd
+# prompts and defaults
+
+class Pirate(Cmd):
+    gold = 3
+    prompt = 'arrr> '
+    def default(self, line):
+        print 'What mean ye by "{0}"?'.format(line)
+    def do_loot(self, arg):
+        'Drown your sorrrows in rrrum.'                
+        self.gold += 1
+    def do_drink(self, arg):
+        '''Drown your sorrrows in rrrum.
+        
+        drink [n] - drink [n] barrel[s] o' rum.'''          
+        try:
+            self.gold -= int(arg)
+        except:
+            if arg:
+                print '''What's "{0}"?  I'll take rrrum.'''.format(arg)
+            self.gold -= 1            
+    def postcmd(self, stop, line):
+        print 'Now we gots {0} doubloons'.format(self.gold)
+        if self.gold < 0:
+            print "Off to debtorrr's prrrison.  Game overrr."
+            return True
+        return stop
+    def do_quit(self, arg):
+        print "Quiterrr!"
+        return True    
+
+pirate = Pirate()
+pirate.cmdloop()
