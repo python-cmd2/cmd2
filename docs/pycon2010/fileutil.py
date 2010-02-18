6fe5fee146c9ec457b809780a070b9fb568cc650
cat <cat@eee>
2010-02-18 16:20:30 -0500

    shorter fileutil

--- docs/pycon2010/fileutil.py
+++ docs/pycon2010/fileutil.py
@@ -1,13 +1,12 @@
-import os
+import glob
 import os.path
 
-for (dirpath, dirnames, filenames) in os.walk('/home/cat/proj/sqlpython/sqlpython'):
-    for fname in filenames:
-        fullfilename = os.path.join(dirpath, fname)
-        stats = os.stat(fullfilename)
-        binds['path'] = dirpath
-        binds['name'] = fname
-        binds['bytes'] = stats.st_size
-        cmd("""INSERT INTO cat.files (path, name, bytes)
-               VALUES (%(path)s, %(name)s, %(bytes)s)""")
-    quit()
+for fullfilename in glob.glob('/home/cat/proj/cmd2/*.py'):
+    dirpath, fname) = os.path.split(fullfilename)
+    stats = os.stat(fullfilename)
+    binds['path'] = dirpath
+    binds['name'] = fname
+    binds['bytes'] = stats.st_size
+    cmd("""INSERT INTO cat.files (path, name, bytes)
+           VALUES (%(path)s, %(name)s, %(bytes)s)""")
+quit()
