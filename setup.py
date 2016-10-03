608ef7a769ba4eb2149053ee6606bb62592e9b5b
Federico Ceratto <federico.ceratto@gmail.com>
2016-10-03 10:53:27 +0100

    Add Python3.6 support

--- setup.py
+++ setup.py
@@ -62,7 +62,7 @@ See docs at http://packages.python.org/cmd2/
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
-
+        'Programming Language :: Python :: 3.6',
     ],
-    )
+)
 
