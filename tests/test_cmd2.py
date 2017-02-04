ffc4da088ecd7bee4f1f3822c35d906b67f3d0f7
Todd Leonhardt <todd.leonhardt@gmail.com>
2017-02-04 17:34:18 -0500

    Added unit test of colorize() method

--- tests/test_cmd2.py
+++ tests/test_cmd2.py
@@ -342,3 +342,15 @@ now: True
     run_cmd(base_app, 'load does_not_exist.txt')
     out, err = capsys.readouterr()
     assert str(err).startswith('Traceback (most recent call last):')
+
+
+def test_base_colorize(base_app):
+    # If using base_app test fixture it won't get colorized because we replaced self.stdout
+    color_test = base_app.colorize('Test', 'red')
+    assert color_test == 'Test'
+
+    # But if we create a fresh Cmd() instance, it will
+    fresh_app = cmd2.Cmd()
+    color_test = fresh_app.colorize('Test', 'red')
+    assert color_test == '\x1b[31mTest\x1b[39m'
+
