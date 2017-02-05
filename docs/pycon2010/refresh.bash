069ecc888e7fb826296a976b21615afe7bb95b89
Todd Leonhardt <todd.leonhardt@gmail.com>
2017-02-05 18:51:34 -0500

    Removed some unused bash scripts which were for uploading stuff to PyCon 2010

--- docs/pycon2010/refresh.bash
+++
@@ -1,1 +0,0 @@
-rst2s5.py --theme-url ui/pycon pycon2010.rst pycon2010.html
--- docs/upload_pycon.bash
+++
@@ -1,2 +0,0 @@
-rst2s5.py --theme-url=pycon2010/ui/pycon pycon2010/pycon2010.rst pycon2010/pycon2010.html  
-scp -r pycon2010/ www-data@$tummy:/var/www/presentations/cmd_cmd2
