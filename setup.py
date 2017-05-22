67e6373838c06d74ec596ed7f880214c2b34c5e6
Todd Leonhardt <todd.leonhardt@gmail.com>
2017-05-22 16:37:53 -0700

    Bumping version to 0.7.3a
    Changing to alpha version just to avoid confusion with release in PyPI

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ Setuptools setup file, used to install or test 'cmd2'
 """
 from setuptools import setup
 
-VERSION = '0.7.2'
+VERSION = '0.7.3a'
 DESCRIPTION = "Extra features for standard library's cmd module"
 
 LONG_DESCRIPTION = """cmd2 is an enhancement to the standard library's cmd module for Python 2.7
