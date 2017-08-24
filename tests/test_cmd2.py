503ea9838d07a8737768e0694fb577d89d0280c2
Jared Crapo <jared@kotfu.net>
2017-08-23 22:10:48 -0600

    Removed unused capsys fixtures

--- tests/test_cmd2.py
+++ tests/test_cmd2.py
@@ -1389,7 +1389,7 @@ def test_echo(capsys):
     assert app._current_script_dir is None
     assert out.startswith('{}{}\n'.format(app.prompt, command) + 'history [arg]: lists past commands issued')
 
-def test_pseudo_raw_input_tty_rawinput_true(capsys):
+def test_pseudo_raw_input_tty_rawinput_true():
     minput = mock.MagicMock(name='input', side_effect=['set', 'quit'])
     sm.input = minput
     mtty = mock.MagicMock(name='isatty', return_value=True)
@@ -1398,9 +1398,7 @@ def test_pseudo_raw_input_tty_rawinput_true(capsys):
     # run the cmdloop, which should pull input from the mocked input
     app = cmd2.Cmd()
     app.use_rawinput = True
-    app.abbrev = False
     app._cmdloop()
-    out, err = capsys.readouterr()
     
     # because we mocked the input() call, we won't see the prompt
     # or the name of the command in the output, so we can't check
@@ -1409,7 +1407,7 @@ def test_pseudo_raw_input_tty_rawinput_true(capsys):
     # that the rest of it worked
     assert minput.call_count == 2
 
-def test_pseudo_raw_input_tty_rawinput_false(capsys):
+def test_pseudo_raw_input_tty_rawinput_false():
     # mock up the input
     fakein = io.StringIO(u'{}'.format('set\nquit\n'))
     mtty = mock.MagicMock(name='isatty', return_value=True)
@@ -1420,9 +1418,7 @@ def test_pseudo_raw_input_tty_rawinput_false(capsys):
     # run the cmdloop, telling it where to get input from
     app = cmd2.Cmd(stdin=fakein)
     app.use_rawinput = False
-    app.abbrev = False
     app._cmdloop()
-    out, err = capsys.readouterr()
     
     # because we mocked the readline() call, we won't see the prompt
     # or the name of the command in the output, so we can't check
