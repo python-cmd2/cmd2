a6f809c554d9c408f2bbe376b07cb1c596d790fc
kotfu <kotfu@kotfu.net>
2018-03-30 09:54:34 -0600

    quoted arguments working

--- tests/test_plyparsing.py
+++ tests/test_plyparsing.py
@@ -14,15 +14,21 @@ class Cmd2Lexer():
     """a ply.lex lexer for the cmd2 syntax.
     Once initial development is completed, this code
     should be moved into cmd2.Cmd()
+
+    Unlike most python classes, the order of the methods matters here. The ply module
+    uses introspection to create an ordered list of grammer rules, so rearranging methods
+    impacts functionality.
     """
     def __init__(self):
         self.results = Cmd2Command()
 
     tokens = (
-        'WORD',
+        'WORD', 'DQWORD', 'SQWORD',
     )
 
-    t_WORD = r"[A-Za-z_]+"
+    t_WORD = r"[-A-z0-9_]+"
+    t_DQWORD = r'"(?:[^"\\]|\\.)*"'
+    t_SQWORD = r"'(?:[^'\\]|\\.)*'"
 
     def t_error(self, t):
         print("Illegal command")
@@ -31,23 +37,26 @@ class Cmd2Lexer():
     def build_lexer(self, **kwargs):
         self.lexer = lex.lex(module=self, **kwargs)
 
-    def p_command_and_args(self, p):
-        'command_and_args : command arglist'
-        self.results.command = p[1]
-        self.results.args = p[2]
-        p[0] = self.results
+    def p_arglist_add_argument(self, p):
+        'command : command word'
+        p[0] = '{} {}'.format(p[1], p[2])
+        self.results.command = p[0]
 
-    def p_command(self, p):
-        'command : WORD'
-        self.results.command = p[1]
+    def p_arglist_argument(self, p):
+        'command : word'
+        p[0] = p[1]
+        self.results.command = p[0]
+
+    def p_argument_word(self, p):
+        'word : WORD'
         p[0] = p[1]
 
-    def p_arglist(self, p):
-        'arglist : arglist WORD'
-        p[0] = '{} {}'.format(p[1], p[2])
+    def p_argument_dqword(self, p):
+        'word : DQWORD'
+        p[0] = p[1]
 
-    def p_arg(self, p):
-        'arglist : WORD'
+    def p_argument_sqword(self, p):
+        'word : SQWORD'
         p[0] = p[1]
 
     def p_error(self, p):
@@ -64,17 +73,32 @@ def cl():
     cl.build_parser()
     return cl
 
-def test_lex_single_word(cl):
+def test_lex_word(cl):
     cl.lexer.input('plainword')
     tok = cl.lexer.token()
     assert tok.type == 'WORD'
     assert tok.value == 'plainword'
+    assert not cl.lexer.token()
 
+def test_lex_dqword(cl):
+    cl.lexer.input('"one word"')
+    tok = cl.lexer.token()
+    assert tok.type == 'DQWORD'
+    assert tok.value == '"one word"'
+    assert not cl.lexer.token()
+
+def test_lex_sqword(cl):
+    cl.lexer.input("'one word'")
+    tok = cl.lexer.token()
+    assert tok.type == 'SQWORD'
+    assert tok.value == "'one word'"
+    assert not cl.lexer.token()
+    
 def test_lex_command_with_args(cl):
-    cl.lexer.input('command with args')
+    cl.lexer.input('123456 with args')
     tok = cl.lexer.token()
     assert tok.type == 'WORD'
-    assert tok.value == 'command'
+    assert tok.value == '123456'
     tok = cl.lexer.token()
     assert tok.type == 'WORD'
     assert tok.value == 'with'
@@ -82,11 +106,26 @@ def test_lex_command_with_args(cl):
     assert tok.type == 'WORD'
     assert tok.value == 'args'
 
-def test_parse_single_word(cl):
+def test_parse_command(cl):
     cl.parser.parse('plainword')
     assert cl.results.command == 'plainword'
 
 def test_parse_command_with_args(cl):
     cl.parser.parse('command with args')
-    assert cl.results.command == 'command'
-    assert cl.results.args == 'with args'
+    assert cl.results.command == 'command with args'
+
+def test_parse_command_with_dqarg(cl):
+    cl.parser.parse('command "with   dqarg"')
+    assert cl.results.command == 'command "with   dqarg"'
+
+def test_parse_command_with_sqarg(cl):
+    cl.parser.parse("command 'with     sqarg'")
+    assert cl.results.command == "command 'with     sqarg'"
+
+def test_parse_command_with_dqarg_and_arg(cl):
+    cl.parser.parse('command "with   dqarg" onemore   lastone')
+    assert cl.results.command == 'command "with   dqarg" onemore lastone'
+
+def test_parse_command_with_sqarg_and_arg(cl):
+    cl.parser.parse("command 'with   dqarg' onemore   lastone")
+    assert cl.results.command == "command 'with   dqarg' onemore lastone"
