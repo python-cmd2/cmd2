dd780edc3d080cd4ae8d62c74006af44ff540a63
kotfu <kotfu@kotfu.net>
2018-04-02 22:20:15 -0600

    Add support for C-style and C++-style comments

--- tests/test_shlexparsing.py
+++ tests/test_shlexparsing.py
@@ -4,17 +4,21 @@ Unit/functional testing for ply based parsing in cmd2
 
 Todo List
 - multiline
-- /* */ style comments
 
 Notes:
 
 - Shortcuts may have to be discarded, or handled in a different way than they
   are with pyparsing.
-- 
+- valid comment styles:
+    - C-style -> /* comment */
+    - C++-style -> // comment
+    - Python/Shell style -> # comment
 
 """
 
+import re
 import shlex
+
 import pytest
 
 class Cmd2Command():
@@ -24,6 +28,21 @@ class Cmd2Parser():
     def parseString(self, rawinput):
         result = Cmd2Command()
         result.raw = rawinput
+
+        # strip C-style and C++-style comments
+        # shlex will handle the python/shell style comments for us
+        def replacer(match):
+                s = match.group(0)
+                if s.startswith('/'):
+                    # treat the removed comment as a space token, not an empty string
+                    return ' '
+                else:
+                    return s
+        pattern = re.compile(
+            r'//.*?$|/\*.*?\*/|\'(?:\\.|[^\\\'])*\'|"(?:\\.|[^\\"])*"',
+            re.DOTALL | re.MULTILINE
+        )
+        rawinput = re.sub(pattern, replacer, rawinput)
 
         s = shlex.shlex(rawinput, posix=False, punctuation_chars=True)
         tokens = list(s)
@@ -38,14 +57,13 @@ class Cmd2Parser():
             # remove all the tokens after the pipe
             tokens = tokens[:pipe_pos-1]
         except ValueError:
+            # no pipe in the tokens
             result.pipeTo = None
 
         if len(tokens) > 1:
             result.args = ' '.join(tokens[1:])
         else:
             result.args = None
-
-
 
         return result
 
@@ -72,6 +90,24 @@ def test_command_with_args(parser):
 
 def test_hashcomment(parser):
     results = parser.parseString('hi # this is all a comment')
+    assert results.command == 'hi'
+    assert not results.args
+    assert not results.pipeTo
+
+def test_c_comment(parser):
+    results = parser.parseString('hi /* this is | all a comment */')
+    assert results.command == 'hi'
+    assert not results.args
+    assert not results.pipeTo
+
+def test_parse_what_if_quoted_strings_seem_to_start_comments(parser):
+    results = parser.parseString('what if "quoted strings /* seem to " start comments?')
+    assert results.command == 'what'
+    assert results.args == 'if "quoted strings /* seem to " start comments?'
+    assert not results.pipeTo
+
+def test_cpp_comment(parser):
+    results = parser.parseString('hi // this is | all a comment */')
     assert results.command == 'hi'
     assert not results.args
     assert not results.pipeTo
