2ff92ee59737c4380f24b4ca6c94822af1131138
kotfu <kotfu@kotfu.net>
2018-04-02 22:01:36 -0600

    Trying out shlex

---
+++ tests/test_shlexparsing.py
@@ -0,0 +1,90 @@
+# coding=utf-8
+"""
+Unit/functional testing for ply based parsing in cmd2
+
+Todo List
+- multiline
+- /* */ style comments
+
+Notes:
+
+- Shortcuts may have to be discarded, or handled in a different way than they
+  are with pyparsing.
+- 
+
+"""
+
+import shlex
+import pytest
+
+class Cmd2Command():
+    pass
+
+class Cmd2Parser():
+    def parseString(self, rawinput):
+        result = Cmd2Command()
+        result.raw = rawinput
+
+        s = shlex.shlex(rawinput, posix=False, punctuation_chars=True)
+        tokens = list(s)
+        result.command = tokens[0]
+
+        # check for pipes
+        try:
+            # find the first pipe if it exists
+            pipe_pos = tokens.index('|')
+            # set everything after the first pipe to result.pipeTo
+            result.pipeTo = ' '.join(tokens[pipe_pos+1:])
+            # remove all the tokens after the pipe
+            tokens = tokens[:pipe_pos-1]
+        except ValueError:
+            result.pipeTo = None
+
+        if len(tokens) > 1:
+            result.args = ' '.join(tokens[1:])
+        else:
+            result.args = None
+
+
+
+        return result
+
+@pytest.fixture
+def parser():
+    parser = Cmd2Parser()
+    return parser
+
+@pytest.mark.parametrize('line', [
+    'plainword',
+    '"one word"',
+    "'one word'",
+])
+def test_single_word(parser, line):
+    results = parser.parseString(line)
+    assert results.command == line
+
+def test_command_with_args(parser):
+    line = 'command with args'
+    results = parser.parseString(line)
+    assert results.command == 'command'
+    assert results.args == 'with args'
+    assert not results.pipeTo
+
+def test_hashcomment(parser):
+    results = parser.parseString('hi # this is all a comment')
+    assert results.command == 'hi'
+    assert not results.args
+    assert not results.pipeTo
+
+def test_simple_piped(parser):
+    results = parser.parseString('simple | piped')
+    assert results.command == 'simple'
+    assert not results.args
+    assert results.pipeTo == 'piped'
+
+def test_double_pipe_is_not_a_pipe(parser):
+    line = 'double-pipe || is not a pipe'
+    results = parser.parseString(line)
+    assert results.command == 'double-pipe'
+    assert results.args == '|| is not a pipe'
+    assert not results.pipeTo
