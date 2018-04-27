8c69e0e75454a5665ed2d428aba3c052b97b68b5
kotfu <kotfu@kotfu.net>
2018-04-27 15:11:31 -0600

    Remove ‘<‘ from REDIRECTION_CHARS, it’s no longer used.

--- cmd2/constants.py
+++ cmd2/constants.py
@@ -6,7 +6,7 @@ import re
 
 # Used for command parsing, tab completion and word breaks. Do not change.
 QUOTES = ['"', "'"]
-REDIRECTION_CHARS = ['|', '<', '>']
+REDIRECTION_CHARS = ['|', '>']
 
 # Regular expression to match ANSI escape codes
 ANSI_ESCAPE_RE = re.compile(r'\x1b[^m]*m')
