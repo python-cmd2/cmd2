2973d1294755312a0f0414d12a7928374f5c7d59
kotfu <kotfu@kotfu.net>
2018-05-15 09:26:17 -0600

    Add Statement to default import

--- cmd2/__init__.py
+++ cmd2/__init__.py
@@ -1,4 +1,4 @@
 #
 # -*- coding: utf-8 -*-
-from .cmd2 import __version__, Cmd, CmdResult, categorize
+from .cmd2 import __version__, Cmd, CmdResult, Statement, categorize
 from .cmd2 import with_argument_list, with_argparser, with_argparser_and_unknown_args, with_category
