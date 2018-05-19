17159bf39da073deebe3d255e2dd017c074a4ac2
Eric Lin <anselor@gmail.com>
2018-05-19 16:00:40 -0400

    Added new command to test validation.

--- tests/test_autocompletion.py
+++ tests/test_autocompletion.py
@@ -168,7 +168,7 @@ def test_autocomp_subcmd_nested(cmd2_app):
 
     first_match = complete_tester(text, line, begidx, endidx, cmd2_app)
     assert first_match is not None and \
-           cmd2_app.completion_matches == ['add', 'delete', 'list']
+           cmd2_app.completion_matches == ['add', 'delete', 'list', 'load']
 
 
 def test_autocomp_subcmd_flag_choices_append(cmd2_app):
