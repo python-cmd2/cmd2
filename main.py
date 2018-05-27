8acfe9b476560249207c3feccec0c20e4eaa2e5d
Todd Leonhardt <todd.leonhardt@gmail.com>
2018-05-27 11:42:31 -0400

    Adding main.py to python2 branch for manual testing with ipy enabled

---
+++ main.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# coding=utf-8
+import cmd2
+
+if __name__ == '__main__':
+    # If run as the main application, simply start a bare-bones cmd2 application with only built-in functionality.
+
+    # Set "use_ipython" to True to include the ipy command if IPython is installed, which supports advanced interactive
+    # debugging of your application via introspection on self.
+    app = cmd2.Cmd(use_ipython=True)
+    app.locals_in_py = True
+    app.cmdloop()
