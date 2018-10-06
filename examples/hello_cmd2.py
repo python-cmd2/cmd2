a77be185c245e4cd04fe348e050a177b9b327471
Todd Leonhardt <todd.leonhardt@gmail.com>
2018-10-06 18:33:25 -0400

    Moved main.py to exampels/hello_cmd2.py and added some comments

--- examples/hello_cmd2.py
+++ examples/hello_cmd2.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
 # coding=utf-8
+"""
+This is intended to be a completely bare-bones cmd2 application suitable for rapid testing and debugging.
+"""
 from cmd2 import cmd2
 
 if __name__ == '__main__':
@@ -8,5 +11,6 @@ if __name__ == '__main__':
     # Set "use_ipython" to True to include the ipy command if IPython is installed, which supports advanced interactive
     # debugging of your application via introspection on self.
     app = cmd2.Cmd(use_ipython=True, persistent_history_file='cmd2_history.txt')
-    app.locals_in_py = True
+    app.locals_in_py = True     # Enable access to "self" within the py command
+    app.debug = True            # Show traceback if/when an exception occurs
     app.cmdloop()
