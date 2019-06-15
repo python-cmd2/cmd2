bedc91d612c478da2ca2f048ddf91180a643c324
Kevin Van Brunt <kmvanbrunt@gmail.com>
2019-06-15 01:07:25 -0400

    Updated unit tests

--- tests/test_pyscript.py
+++ tests/test_pyscript.py
@@ -15,7 +15,7 @@ def cmdfinalization_hook(data: plugin.CommandFinalizationData) -> plugin.Command
     print(HOOK_OUTPUT)
     return data
 
-def test_pyscript_help(base_app, request):
+def test_run_pyscript_help(base_app, request):
     test_dir = os.path.dirname(request.module.__file__)
     python_script = os.path.join(test_dir, 'pyscript', 'help.py')
     out1, err1 = run_cmd(base_app, 'help')
@@ -23,7 +23,7 @@ def test_pyscript_help(base_app, request):
     assert out1 and out1 == out2
 
 
-def test_pyscript_dir(base_app, request):
+def test_run_pyscript_dir(base_app, request):
     test_dir = os.path.dirname(request.module.__file__)
     python_script = os.path.join(test_dir, 'pyscript', 'pyscript_dir.py')
 
@@ -32,7 +32,7 @@ def test_pyscript_dir(base_app, request):
     assert out[0] == "['cmd_echo']"
 
 
-def test_pyscript_stdout_capture(base_app, request):
+def test_run_pyscript_stdout_capture(base_app, request):
     base_app.register_cmdfinalization_hook(cmdfinalization_hook)
     test_dir = os.path.dirname(request.module.__file__)
     python_script = os.path.join(test_dir, 'pyscript', 'stdout_capture.py')
@@ -41,7 +41,7 @@ def test_pyscript_stdout_capture(base_app, request):
     assert out[0] == "PASSED"
     assert out[1] == "PASSED"
 
-def test_pyscript_stop(base_app, request):
+def test_run_pyscript_stop(base_app, request):
     # Verify onecmd_plus_hooks() returns True if any commands in a pyscript return True for stop
     test_dir = os.path.dirname(request.module.__file__)
 
