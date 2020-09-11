6093e5e9c1b6366c67323f090d21696e867b6625
Eric Lin <anselor@gmail.com>
2020-09-11 09:25:39 -0400

    Fixed classifier spacing

--- plugins/ext_test/setup.py
+++ plugins/ext_test/setup.py
@@ -48,8 +48,8 @@ setuptools.setup(
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
-        'Programming Language:: Python:: 3.8',
-        'Programming Language:: Python:: 3.9',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
     ],
 
     # dependencies for development and testing
