279cfda015e8bef231cbce52c8ae1b7b4ce14edf
Todd Leonhardt <todd.leonhardt@gmail.com>
2021-06-14 22:57:04 -0400

    Add CI testing on Python 3.10 beta2 (#1122)
    * Begin testing on Python 3.10 beta 2
    
    * Try to fix 3.10 build
    
    * Add nox session

--- noxfile.py
+++ noxfile.py
@@ -17,7 +17,7 @@ def docs(session):
     )
 
 
-@nox.session(python=['3.6', '3.7', '3.8', '3.9'])
+@nox.session(python=['3.6', '3.7', '3.8', '3.9', '3.10.0-beta.2'])
 @nox.parametrize('plugin', [None, 'ext_test', 'template', 'coverage'])
 def tests(session, plugin):
     if plugin is None:
