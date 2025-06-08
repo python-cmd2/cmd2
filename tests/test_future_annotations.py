2c91df00691ce92c4488f0302fba884cb1d11dd6
Todd Leonhardt <todd.leonhardt@gmail.com>
2025-06-08 17:28:09 -0400

    Added a new test file to cover the case representing the whole reason for this change
    I first verified that this test fails on the current master branch

---
+++ tests/test_future_annotations.py
@@ -0,0 +1,22 @@
+from __future__ import annotations
+
+import cmd2
+
+from .conftest import normalize, run_cmd
+
+
+def test_hooks_work_with_future_annotations() -> None:
+    class HookApp(cmd2.Cmd):
+        def __init__(self, *args, **kwargs) -> None:
+            super().__init__(*args, **kwargs)
+            self.register_cmdfinalization_hook(self.hook)
+
+        def hook(self: cmd2.Cmd, data: cmd2.plugin.CommandFinalizationData) -> cmd2.plugin.CommandFinalizationData:
+            if self.in_script():
+                self.poutput("WE ARE IN SCRIPT")
+            return data
+
+    hook_app = HookApp()
+    out, err = run_cmd(hook_app, '')
+    expected = normalize('')
+    assert out == expected
