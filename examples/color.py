38927240542bc7622d1362240b8200e4e3ca6e54
Kevin Van Brunt <kmvanbrunt@gmail.com>
2025-08-21 01:21:51 -0400

    Simplified color example.

--- examples/color.py
+++ examples/color.py
@@ -4,10 +4,15 @@
 Execute the taste_the_rainbow command to see the colors available.
 """
 
+import argparse
+
 from rich.style import Style
 
 import cmd2
-from cmd2.colors import Color
+from cmd2 import (
+    Color,
+    stylize,
+)
 
 
 class CmdLineApp(cmd2.Cmd):
@@ -19,15 +24,16 @@ class CmdLineApp(cmd2.Cmd):
         self.intro = 'Run the taste_the_rainbow command to see all of the colors available to you in cmd2.'
 
     rainbow_parser = cmd2.Cmd2ArgumentParser()
-    rainbow_parser.add_argument('-b', '--background', action='store_true', help='Show background colors as well')
+    rainbow_parser.add_argument('-b', '--background', action='store_true', help='show background colors as well')
 
     @cmd2.with_argparser(rainbow_parser)
-    def do_taste_the_rainbow(self, args) -> None:
+    def do_taste_the_rainbow(self, args: argparse.Namespace) -> None:
         """Show all of the colors available within cmd2's Color StrEnum class."""
 
         for color_member in Color:
-            style = Style(bgcolor=color_member.value) if args.background else Style(color=color_member.value)
-            self.poutput(f"{color_member.name}", style=style, soft_wrap=False)
+            style = Style(bgcolor=color_member) if args.background else Style(color=color_member)
+            styled_name = stylize(color_member.name, style=style)
+            self.poutput(styled_name)
 
 
 if __name__ == '__main__':
