88b350b8294c8eb9965341e7bc1f92f6f665dd1d
Todd Leonhardt <todd.leonhardt@gmail.com>
2025-09-16 19:38:51 -0400

    Move two test files that don't need to be isolated from tests_isolated to tests
    Also:
    - Remove unnecessary nested directory under tests_isolated for the one remaining test file there.
    
    This is the first step in fixing the tests so none of them need to be isolated.  The tests in tests_isolated/test_commandset.py follow bad practices where classes are modified instead of instances of classes.

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,6 +5,7 @@ import sys
 from collections.abc import Callable
 from contextlib import redirect_stderr
 from typing import (
+    TYPE_CHECKING,
     ParamSpec,
     TextIO,
     TypeVar,
@@ -164,3 +165,71 @@ def find_subcommand(action: argparse.ArgumentParser, subcmd_names: list[str]) ->
                     return find_subcommand(choice, subcmd_names)
             break
     raise ValueError(f"Could not find subcommand '{subcmd_names}'")
+
+
+if TYPE_CHECKING:
+    _Base = cmd2.Cmd
+else:
+    _Base = object
+
+
+class ExternalTestMixin(_Base):
+    """A cmd2 plugin (mixin class) that exposes an interface to execute application commands from python"""
+
+    def __init__(self, *args, **kwargs):
+        """
+
+        :type self: cmd2.Cmd
+        :param args:
+        :param kwargs:
+        """
+        # code placed here runs before cmd2 initializes
+        super().__init__(*args, **kwargs)
+        assert isinstance(self, cmd2.Cmd)
+        # code placed here runs after cmd2 initializes
+        self._pybridge = cmd2.py_bridge.PyBridge(self)
+
+    def app_cmd(self, command: str, echo: bool | None = None) -> cmd2.CommandResult:
+        """
+        Run the application command
+
+        :param command: The application command as it would be written on the cmd2 application prompt
+        :param echo: Flag whether the command's output should be echoed to stdout/stderr
+        :return: A CommandResult object that captures stdout, stderr, and the command's result object
+        """
+        assert isinstance(self, cmd2.Cmd)
+        assert isinstance(self, ExternalTestMixin)
+        try:
+            self._in_py = True
+
+            return self._pybridge(command, echo=echo)
+
+        finally:
+            self._in_py = False
+
+    def fixture_setup(self):
+        """
+        Replicates the behavior of `cmdloop()` preparing the state of the application
+        :type self: cmd2.Cmd
+        """
+
+        for func in self._preloop_hooks:
+            func()
+        self.preloop()
+
+    def fixture_teardown(self):
+        """
+        Replicates the behavior of `cmdloop()` tearing down the application
+
+        :type self: cmd2.Cmd
+        """
+        for func in self._postloop_hooks:
+            func()
+        self.postloop()
+
+
+class WithCommandSets(ExternalTestMixin, cmd2.Cmd):
+    """Class for testing custom help_* methods which override docstring help."""
+
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
