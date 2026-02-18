c21bdb115bb950f4bc7c454a0dc6c3de23265f09
Kevin Van Brunt <kmvanbrunt@gmail.com>
2026-02-18 00:37:42 -0500

    Removed flag_based_complete and index_based_complete functions.

--- examples/basic_completion.py
+++ examples/basic_completion.py
@@ -1,24 +1,18 @@
 #!/usr/bin/env python
 """A simple example demonstrating how to enable tab completion by assigning a completer function to do_* commands.
+
 This also demonstrates capabilities of the following completer features included with cmd2:
 - CompletionError exceptions
 - delimiter_complete()
-- flag_based_complete() (see note below)
-- index_based_complete() (see note below).
 
-flag_based_complete() and index_based_complete() are basic methods and should only be used if you are not
-familiar with argparse. The recommended approach for tab completing positional tokens and flags is to use
-argparse-based completion. For an example integrating tab completion with argparse, see argparse_completion.py
+The recommended approach for tab completing is to use argparse-based completion.
+For an example integrating tab completion with argparse, see argparse_completion.py.
 """
 
 import functools
+from typing import NoReturn
 
 import cmd2
-from cmd2 import Completions
-
-# List of strings used with completion functions
-food_item_strs = ['Pizza', 'Ham', 'Ham Sandwich', 'Potato']
-sport_item_strs = ['Bat', 'Basket', 'Basketball', 'Football', 'Space Ball']
 
 # This data is used to demonstrate delimiter_complete
 file_strs = [
@@ -34,44 +28,6 @@ class BasicCompletion(cmd2.Cmd):
     def __init__(self) -> None:
         super().__init__(auto_suggest=False, include_py=True)
 
-    def do_flag_based(self, statement: cmd2.Statement) -> None:
-        """Tab completes arguments based on a preceding flag using flag_based_complete
-        -f, --food [completes food items]
-        -s, --sport [completes sports]
-        -p, --path [completes local file system paths].
-        """
-        self.poutput(f"Args: {statement.args}")
-
-    def complete_flag_based(self, text, line, begidx, endidx) -> Completions:
-        """Completion function for do_flag_based."""
-        flag_dict = {
-            # Tab complete food items after -f and --food flags in command line
-            '-f': food_item_strs,
-            '--food': food_item_strs,
-            # Tab complete sport items after -s and --sport flags in command line
-            '-s': sport_item_strs,
-            '--sport': sport_item_strs,
-            # Tab complete using path_complete function after -p and --path flags in command line
-            '-p': self.path_complete,
-            '--path': self.path_complete,
-        }
-
-        return self.flag_based_complete(text, line, begidx, endidx, flag_dict=flag_dict)
-
-    def do_index_based(self, statement: cmd2.Statement) -> None:
-        """Tab completes first 3 arguments using index_based_complete."""
-        self.poutput(f"Args: {statement.args}")
-
-    def complete_index_based(self, text, line, begidx, endidx) -> Completions:
-        """Completion function for do_index_based."""
-        index_dict = {
-            1: food_item_strs,  # Tab complete food items at index 1 in command line
-            2: sport_item_strs,  # Tab complete sport items at index 2 in command line
-            3: self.path_complete,  # Tab complete using path_complete function at index 3 in command line
-        }
-
-        return self.index_based_complete(text, line, begidx, endidx, index_dict=index_dict)
-
     def do_delimiter_complete(self, statement: cmd2.Statement) -> None:
         """Tab completes files from a list using delimiter_complete."""
         self.poutput(f"Args: {statement.args}")
@@ -83,7 +39,7 @@ class BasicCompletion(cmd2.Cmd):
         """Demonstrates effect of raising CompletionError."""
         self.poutput(f"Args: {statement.args}")
 
-    def complete_raise_error(self, _text, _line, _begidx, _endidx) -> Completions:
+    def complete_raise_error(self, _text: str, _line: str, _begidx: int, _endidx: int) -> NoReturn:
         """CompletionErrors can be raised if an error occurs while tab completing.
 
         Example use cases
--- examples/modular_commands/commandset_basic.py
+++ examples/modular_commands/commandset_basic.py
@@ -11,10 +11,6 @@ from cmd2 import (
 
 @with_default_category('Basic Completion')
 class BasicCompletionCommandSet(CommandSet):
-    # List of strings used with completion functions
-    food_item_strs = ('Pizza', 'Ham', 'Ham Sandwich', 'Potato')
-    sport_item_strs = ('Bat', 'Basket', 'Basketball', 'Football', 'Space Ball')
-
     # This data is used to demonstrate delimiter_complete
     file_strs = (
         '/home/user/file.db',
@@ -23,44 +19,6 @@ class BasicCompletionCommandSet(CommandSet):
         '/home/other user/maps.db',
         '/home/other user/tests.db',
     )
-
-    def do_flag_based(self, statement: Statement) -> None:
-        """Tab completes arguments based on a preceding flag using flag_based_complete
-        -f, --food [completes food items]
-        -s, --sport [completes sports]
-        -p, --path [completes local file system paths].
-        """
-        self._cmd.poutput(f"Args: {statement.args}")
-
-    def complete_flag_based(self, text: str, line: str, begidx: int, endidx: int) -> list[str]:
-        """Completion function for do_flag_based."""
-        flag_dict = {
-            # Tab complete food items after -f and --food flags in command line
-            '-f': self.food_item_strs,
-            '--food': self.food_item_strs,
-            # Tab complete sport items after -s and --sport flags in command line
-            '-s': self.sport_item_strs,
-            '--sport': self.sport_item_strs,
-            # Tab complete using path_complete function after -p and --path flags in command line
-            '-p': self._cmd.path_complete,
-            '--path': self._cmd.path_complete,
-        }
-
-        return self._cmd.flag_based_complete(text, line, begidx, endidx, flag_dict=flag_dict)
-
-    def do_index_based(self, statement: Statement) -> None:
-        """Tab completes first 3 arguments using index_based_complete."""
-        self._cmd.poutput(f"Args: {statement.args}")
-
-    def complete_index_based(self, text: str, line: str, begidx: int, endidx: int) -> list[str]:
-        """Completion function for do_index_based."""
-        index_dict = {
-            1: self.food_item_strs,  # Tab complete food items at index 1 in command line
-            2: self.sport_item_strs,  # Tab complete sport items at index 2 in command line
-            3: self._cmd.path_complete,  # Tab complete using path_complete function at index 3 in command line
-        }
-
-        return self._cmd.index_based_complete(text, line, begidx, endidx, index_dict=index_dict)
 
     def do_delimiter_complete(self, statement: Statement) -> None:
         """Tab completes files from a list using delimiter_complete."""
