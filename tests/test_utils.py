44fd9e86dea50d97e0914266bb0748d4a15e4d9b
Kevin Van Brunt <kmvanbrunt@gmail.com>
2026-05-27 22:40:55 -0400

    Added test coverage.

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -396,3 +396,37 @@ def test_get_types_method() -> None:
     param_name, param_value = next(iter(param_ann.items()))
     assert param_name == "x"
     assert param_value is bool
+
+
+def test_categorize() -> None:
+    from cmd2 import constants
+
+    category = "Test Category"
+    attr_name = constants.COMMAND_ATTR_HELP_CATEGORY
+
+    # Test single function
+    def func1() -> None:
+        pass
+
+    cu.categorize(func1, category)
+    assert getattr(func1, attr_name) == category
+
+    # Test iterable of functions
+    def func2() -> None:
+        pass
+
+    def func3() -> None:
+        pass
+
+    cu.categorize([func2, func3], category)
+    assert getattr(func2, attr_name) == category
+    assert getattr(func3, attr_name) == category
+
+    # Test bound method
+    class Foo:
+        def bar(self) -> None:
+            pass
+
+    f = Foo()
+    cu.categorize(f.bar, category)
+    assert getattr(Foo.bar, attr_name) == category
